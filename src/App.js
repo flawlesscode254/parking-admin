41a2b619fef41eadca0a70e30e3236452b73e57c
flawlesscode254 <denzme414@gmail.com>
2021-03-02 17:40:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase'
+import React, { useState } from 'react'
+
+
+var firebaseApp =  firebase.initializeApp({
+  apiKey: "AIzaSyCtdvgJ1tJ72f7HCcvth-XDnbG35l2OjJM",
+  authDomain: "car-parking-f77b8.firebaseapp.com",
+  projectId: "car-parking-f77b8",
+  storageBucket: "car-parking-f77b8.appspot.com",
+  messagingSenderId: "811896733090",
+  appId: "1:811896733090:web:1b75f4491b5cdbbcdac6fb",
+  measurementId: "G-LLZPT4LFRK"
+  })
+
+  const db = firebaseApp.firestore()
 
 function App() {
+  const [view, setView] = useState('')
+  const [name, setName] = useState([])
+  const [phone, setPhone] = useState([])
+  const [plate, setPlate] = useState([])
+
+  const handleChange = () => {
+    db.collection("users").where("code", "==", Number(view)).get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+          setName(doc.data().name)
+          setPhone(doc.data().phone)
+          setPlate(doc.data().plate)
+      })
+  })
+  setView('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Enter the unique code" value={view}
+      onChange={(e) => setView(e.target.value)} />
+      <br/>
+      <br/>
+      <button onClick={handleChange}>Submit</button>
+      <br/>
+      <br/>
+      <p>{name}</p>
+      <p>{phone}</p>
+      <p>{plate}</p>
     </div>
   );
 }
